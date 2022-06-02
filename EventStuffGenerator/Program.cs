8bd02ad568aa88bc36a1a2fb2fe2b485083c154a
cplkimth <cplkimth@naver.com>
2022-06-02 12:38:15 +0900

    .

--- EventStuffGenerator/Program.cs
+++ EventStuffGenerator/Program.cs
@@ -23,32 +23,32 @@ namespace EventStuffGenerator
 
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(o =>
-                            {
-                                var template = string.IsNullOrEmpty(o.Template) ? Template.DefaultName : o.Template;
-                                var templatePath = $"{Template.RootPath}/{template}{Template.FileExtension}";
+            {
+                var template = string.IsNullOrEmpty(o.Template) ? Template.DefaultName : o.Template;
+                var templatePath = $"{Template.RootPath}/{template}{Template.FileExtension}";
 
-                                var code = Generator.Generate(templatePath, o.EventName, o.ArgumentText);
-                                Console.WriteLine(code);
+                var code = Generator.Generate(templatePath, o.EventName, o.ArgumentText);
+                Console.WriteLine(code);
 
-                                Console.WriteLine("상기 코드를 클립보드로 복사할까요?\n*주의* 기존 클립보드의 내용이 지워집니다. ([y]/n)");
-                                var copyToClipboard = Console.ReadLine().ToLower();
+                Console.WriteLine("상기 코드를 클립보드로 복사할까요?\n*주의* 기존 클립보드의 내용이 지워집니다. ([y]/n)");
+                var copyToClipboard = Console.ReadLine().ToLower();
 
-                                if (copyToClipboard == "n")
-                                    return;
+                if (copyToClipboard == "n")
+                    return;
 
-                                try
-                                {
-                                    ClipboardService.SetText(code);
-                                    Console.WriteLine("클립보드로 복사하였습니다.");
-                                }
-                                catch (Exception ex)
-                                {
-                                    Console.WriteLine("[예외]");
-                                    Console.WriteLine(ex.Message);
-                                    Console.WriteLine("리눅스라면 아래 명령어를 통해 xclip을 설치하세요.");
-                                    Console.WriteLine("sudo apt install xclip");
-                                }
-                            });
+                try
+                {
+                    ClipboardService.SetText(code);
+                    Console.WriteLine("클립보드로 복사하였습니다.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("[예외]");
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("리눅스라면 아래 명령어를 통해 xclip을 설치하세요.");
+                    Console.WriteLine("sudo apt install xclip");
+                }
+            });
         }
 
         public class Options
